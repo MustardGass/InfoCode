965de08094a56da7f9876a79fd3cbec8d639b9f1
Hugo <101109194+MustardGass@users.noreply.github.com>
2024-04-09 19:21:31 +0200

    Commit dubte
    Que està passant aqui

--- Projecte actualitzat/app/Views/pages/TicketProfessors.php
+++ Projecte actualitzat/app/Views/pages/TicketProfessors.php
@@ -4,65 +4,63 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <script src= "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js"> </script> 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css"> 
-    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js"> </script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
   <title>Assignats</title>
 
 
-  <script type="text/javascript"> 
-    function showHideRow(row) { 
-        $("#" + row).toggle(); 
+  <script type="text/javascript">
+    function showHideRow(row) {
+      $("#" + row).toggle();
     } 
-</script> 
+  </script>
 
 
-<style> 
-    
-    #wrapper { 
-        margin: 0 auto; 
-        padding: 0px; 
-        text-align: center; 
-    } 
+  <style>
+    #wrapper {
+      margin: 0 auto;
+      padding: 0px;
+      text-align: center;
+    }
 
-    #wrapper h1 { 
-        margin-top: 50px; 
-        font-size: 45px; 
-        color: #585858; 
-    } 
+    #wrapper h1 {
+      margin-top: 50px;
+      font-size: 45px;
+      color: #585858;
+    }
 
-    #wrapper h1 p { 
-        font-size: 20px; 
-    } 
+    #wrapper h1 p {
+      font-size: 20px;
+    }
 
-    #table_detail { 
-        max-width: 100%;
-        text-align: left; 
-        border-collapse: collapse; 
-        color: #2E2E2E; 
-    } 
+    #table_detail {
+      max-width: 100%;
+      text-align: left;
+      border-collapse: collapse;
+      color: #2E2E2E;
+    }
 
-    #table_detail tr:hover { 
-        background-color: #000000; 
-    } 
+    #table_detail tr:hover {
+      background-color: #000000;
+    }
 
-    #table_detail .hidden_row { 
-        display: none; 
-        max-width: 100%;
-    } 
+    #table_detail .hidden_row {
+      display: none;
+      max-width: 100%;
+    }
 
-    .headerColor{
-    background-color: #005187;
-}
+    .headerColor {
+      background-color: #005187;
+    }
 
-.menuEsquerra{
-    background-color: #344450;
-}
-
-    
-</style> 
+    .menuEsquerra {
+      background-color: #344450;
+    }
+  </style>
 
 
 </head>
@@ -71,7 +69,7 @@
 
   <nav class="navbar navbar-expand-lg navbar-light headerColor ">
     <div class="container-fluid">
-      <a> <img src=<?= base_url('img/logo.png');?> alt="Logo" style="max-height: 50px;"> </a>
+      <a> <img src=<?= base_url('img/logo.png'); ?> alt="Logo" style="max-height: 50px;"> </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -79,23 +77,24 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-          <a class="nav-link active text-white" aria-current="page" href="#"><?= lang('TicketProfessors.sstt_header');?></a>
+            <a class="nav-link active text-white" aria-current="page"
+              href="#"><?= lang('TicketProfessors.sstt_header'); ?></a>
           </li>
         </ul>
         <div class="d-flex">
           <li class="nav-item dropdown d-flex ">
             <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button"
               data-bs-toggle="dropdown" aria-expanded="false">
-              <img src=<?= base_url('img/user.png');?> alt="User" style="max-height: 30px;">
-              <?= lang('TicketProfessors.usuari');?>
+              <img src=<?= base_url('img/user.png'); ?> alt="User" style="max-height: 30px;">
+              <?= lang('TicketProfessors.usuari'); ?>
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#"><?= lang('TicketProfessors.opt1');?></a></li>
-              <li><a class="dropdown-item" href="#"><?= lang('TicketProfessors.opt2');?></a></li>
+              <li><a class="dropdown-item" href="#"><?= lang('TicketProfessors.opt1'); ?></a></li>
+              <li><a class="dropdown-item" href="#"><?= lang('TicketProfessors.opt2'); ?></a></li>
               <li>
                 <hr class="dropdown-divider">
               </li>
-              <li><a class="dropdown-item" href="#"><?= lang('TicketProfessors.desconnectar');?></a></li>
+              <li><a class="dropdown-item" href="#"><?= lang('TicketProfessors.desconnectar'); ?></a></li>
             </ul>
           </li>
         </div>
@@ -104,87 +103,79 @@
   </nav>
 
   <div class="container-fluid">
-    
+
     <div class="row ">
       <!-- MENÚ VERTICAL ESQUERRA -->
-      <div class="container-fluid">
-    <div class="row flex-nowrap">
-    <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-    <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start ms-0 text-start" id="menu">
-      <li class="nav-item mt-3">
-        <a class="nav-link text-white" href="#"> <img src=<?= base_url('img/iconMenuRosca.png');?> alt="Logo" style="max-height: 30px;"><?= lang('TicketProfessors.reparacions_menu');?></a>
-      </li>
-                    
-      <li class="nav-item mt-3">
-        <a class="nav-link text-white" href="#"> <img src=<?= base_url('img/iconInventari.png');?> alt="Logo" style="max-height: 30px;"><?= lang('TicketProfessors.inventari_menu');?></a>
-      </li>
-    </ul>          
-  </div>
-</div>
+      <div class="container-fluid ps-0">
+        <div class="row flex-nowrap">
+          <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+              <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start ms-0 text-start" id="menu">
+                <li class="nav-item mt-3">
+                  <a class="nav-link text-white" href="#"> <img src=<?= base_url('img/iconMenuRosca.png'); ?> alt="Logo"
+                      style="max-height: 30px;"><?= lang('TicketProfessors.reparacions_menu'); ?></a>
+                </li>
+
+                <li class="nav-item mt-3">
+                  <a class="nav-link text-white" href="#"> <img src=<?= base_url('img/iconInventari.png'); ?> alt="Logo"
+                      style="max-height: 30px;"><?= lang('TicketProfessors.inventari_menu'); ?></a>
+                </li>
+              </ul>
+            </div>
+          </div>
 
 
-<div class="col py-3">
+          <div class="col py-3">
 
-  <div class="mt-3 mb-5">
-    <h1><?= lang('TicketProfessors.titol_reparacions');?></h1>
-  </div>
+            <div class="mt-3 mb-5">
+              <h1><?= lang('TicketProfessors.titol_reparacions'); ?></h1>
+            </div>
 
-  <div class='mb-3'>
-      <nav class="navbar navbar-light">
-            <form class="form-inline">
-              <input class="form-control mr-sm-2 border border-primary rounded-pill" type="search" placeholder="<?= lang('TicketProfessors.cerca');?>" aria-label="Search">
-              <img style="height:30px;" src=<?= base_url('img/filtre.png');?>>
-              <!-- <a class="btn ms-2" style="background-color:#86eba9;"><img style="height:20px; margin-bottom: 3px;" src=<?= base_url('img/afegir.png');?>>&nbsp <?= lang('TicketProfessors.afegir_button');?></a> -->
-            </form>
-
-      </nav>
-  </div>
     
+            <!-- <table  class="table" id="table_detail" cellpadding=10> 
 
 
-  <!-- <table  class="table" id="table_detail" cellpadding=10> 
+            <tr> 
+                <th scope="col" style='background-color: #4d82bc;'><input type="checkbox"/></th>
+                <th scope="col" style='background-color: #4d82bc;'><?= lang('TicketProfessors.codi_taula'); ?></th>
+                <th scope="col" style='background-color: #4d82bc;'><?= lang('TicketProfessors.equip_taula'); ?></th>
+                <th scope="col" style='background-color: #4d82bc;'><?= lang('TicketProfessors.supervisor_taula'); ?></th>
+                <th scope="col" style='background-color: #4d82bc;'><?= lang('TicketProfessors.dia_entrada_taula'); ?></th>
+                <th scope="col" style='background-color: #4d82bc;'><?= lang('TicketProfessors.dia_sortida_taula'); ?></th>
+                <th scope="col" style='background-color: #4d82bc;'><?= lang('TicketProfessors.estat_taula'); ?></th>
+                <th scope="col" style='background-color: #4d82bc;'></th>
+            </tr> 
 
 
-    <tr> 
-        <th scope="col" style='background-color: #4d82bc;'><input type="checkbox"/></th>
-        <th scope="col" style='background-color: #4d82bc;'><?= lang('TicketProfessors.codi_taula');?></th>
-        <th scope="col" style='background-color: #4d82bc;'><?= lang('TicketProfessors.equip_taula');?></th>
-        <th scope="col" style='background-color: #4d82bc;'><?= lang('TicketProfessors.supervisor_taula');?></th>
-        <th scope="col" style='background-color: #4d82bc;'><?= lang('TicketProfessors.dia_entrada_taula');?></th>
-        <th scope="col" style='background-color: #4d82bc;'><?= lang('TicketProfessors.dia_sortida_taula');?></th>
-        <th scope="col" style='background-color: #4d82bc;'><?= lang('TicketProfessors.estat_taula');?></th>
-        <th scope="col" style='background-color: #4d82bc;'></th>
-    </tr> 
+            </table> -->
 
+            <div>
 
-  </table> -->
-
-
-  <?= $table ?>
+              <?= $table ?>
+            </div>
 
 
 
 
-</div>
+          </div>
 
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  <script>
-    function mostrarInformacion(id) {
-      //Obtenim la info addicional
-      var infoElement = document.getElementById('info-' + id);
-  
-      //Alterna etre dislay none i que el display sigui "true"
-      if (infoElement.style.display === 'none' || infoElement.style.display === '') {
-        //Si està ocult, mostrar
-        infoElement.style.display = 'block';
-      } else {
-        // Si està visible, ocultar
-        infoElement.style.display = 'none';
-      }
-    }
-  </script>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+          <script>
+            function mostrarInformacion(id) {
+              //Obtenim la info addicional
+              var infoElement = document.getElementById('info-' + id);
+
+              //Alterna etre dislay none i que el display sigui "true"
+              if (infoElement.style.display === 'none' || infoElement.style.display === '') {
+                //Si està ocult, mostrar
+                infoElement.style.display = 'block';
+              } else {
+                // Si està visible, ocultar
+                infoElement.style.display = 'none';
+              }
+            }
+          </script>
 </body>
 
 </html>
