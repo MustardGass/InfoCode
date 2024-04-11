7697b898cafe0115471f0ea8c38e7263b1fba36a
Hugo <101109194+MustardGass@users.noreply.github.com>
2024-04-11 20:30:09 +0200

    Update RolsModel

--- Projecte actualitzat/app/Models/RolsModel.php
+++ Projecte actualitzat/app/Models/RolsModel.php
@@ -12,7 +12,7 @@ class RolsModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = ['id_rol', 'rol'];
+    protected $allowedFields    = ['id_rol', 'tipus_rol'];
 
     protected bool $allowEmptyInserts = false;
 
