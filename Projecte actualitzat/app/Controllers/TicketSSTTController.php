83dbff52ead7cc75348529b248763e9b54a88f83
andybh21 <abenites@inscaparrella.cat>
2024-04-13 21:00:42 +0200

    tiquetSSTT tabla vista hecha

--- Projecte actualitzat/app/Controllers/TicketSSTTController.php
+++ Projecte actualitzat/app/Controllers/TicketSSTTController.php
@@ -2,41 +2,69 @@
 
 namespace App\Controllers;
 use App\Models\TiquetModel;
+use App\Models\TipusDispositiuModel;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
 use SIENSIS\KpaCrud\Libraries\KpaCrud;
 
 class TicketSSTTController extends BaseController
 {
     public function vista_ticket_sstt()
     {
-        // Model tiquet
-        $tiquetModel = new TiquetModel();
+       // Model tiquet
+       $tiquetModel = new TiquetModel();
+       $modelTipus_dispositiu = new TipusDispositiuModel();
 
-        // Configurar KpaCrud
-        $config = [
-            'editable' => true,
-            'removable' => true,
-            // hi ha més configuració ademés d'aquesta
-        ];
+       // Configurar KpaCrud
+       $config = [
+           'numerate' => false,
+           'editable' => true,
+           'removable' => true,
+           'add_button' => true,
+           // hi ha més configuració ademés d'aquesta
+       ];
 
-        // Crear instancia KpaCrud
-        $crud = new KpaCrud();
-        $crud->setConfig($config);
+       // Crear instancia KpaCrud
+       $crud = new KpaCrud();
+       $crud->setConfig($config);
 
-        $data['tiquets'] = $tiquetModel->obtenirTiquets();
+       // $tipus_tiquet = $tiquetModel->obtindreID();
 
-        $crud->setTable('tiquet'); // Nom de la taula
-        $crud->setPrimaryKey('id_tiquet'); // Clau primària
+       $crud->setTable('tiquet'); // Nom de la taula
+       $crud->setPrimaryKey('id_tiquet'); // Clau primària
+       $crud->setRelation('idFK_dispositiu', 'tipus_dispositiu', 'id_tipus', 'tipus'); //relacio entre taula tiquet i tipus_dispositiu
+       
+       // Columnes que volem veure
+       $crud->setColumns(['id_tiquet', 'tipus_dispositiu__tipus', 'descripcio_avaria', 'centre_emitent', 'centre_reparador', 'codi_equip', 'data_alta', 'estat_tiquet']);
+       $crud->setColumnsInfo([
+           'id_tiquet' => [
+               'name' => 'Codi del tiquet'
+           ],
+           'tipus_dispositiu__tipus' => [
+               'name' => 'Tipus de dispositiu'
+           ],
+           'descripcio_avaria' => [
+               'name' => 'Descripció'
+           ],
+           'centre_emitent' => [
+               'name' => 'Centre emisor'
+           ],
+           'centre_reparador' => [
+               'name' => 'Centre taller'
+           ],
+           'data_alta' => [
+               'name' => 'Data creació'
+           ],
+           'estat' => [
+               'name' => 'Estat'
+           ]
+       ]);
 
-        // Columnes que volem veure
-        $crud->setColumns(['codi_equip', 'idFK_dispositiu', 'idFK_codiCentre']);
 
-        // Generar la taula KpaCrud
-        $data['table'] = $crud->render();
+       // Generar la taula KpaCrud
+       $data['table'] = $crud->render();
 
-        // Passar dades a la vista
-        return view('pages/TicketSSTT', $data);
+       // Passar dades a la vista
+       return view('pages/TicketSSTT', $data);
     }
 }
