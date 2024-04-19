4f23825c5a8af401ec215b88dcfe7492e05d54dc
Hugo <101109194+MustardGass@users.noreply.github.com>
2024-04-19 20:18:20 +0200

    cambios en la conexion HAY QUE CAMBIARLO POR LO COMENTADO

--- Projecte actualitzat/app/Config/App.php
+++ Projecte actualitzat/app/Config/App.php
@@ -17,6 +17,7 @@ class App extends BaseConfig
      *    http://example.com/
      */
     public string $baseURL = 'http://localhost';
+    // public string $baseURL = 'https://kpatek5.capalabs.com';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
